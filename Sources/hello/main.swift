be2c7748a85bb6dc933d0e3431e4261d05c5d60d
Bjørn Einar Bjartnes <bjartnes@gmail.com>
2021-04-09 19:41:39 +0200

    apply compiles but Iam not sure it works

--- Sources/hello/main.swift
+++ Sources/hello/main.swift
@@ -5,11 +5,20 @@ func add(_ x: Int) -> (_ y: Int) -> Int {
     { y in return x + y }
 }
 
+let addTwo = add(2)
+
+print(addTwo(3))
+print(addTwo(5))
+
+
 // https://github.com/nrkno/fsharpskolen/blob/master/ddd-fsharp/functional/apply.fsx#L32
 // This is option.pure but I do not know how to make an option module in swift yet 
-
 func pure<T>(_ x: T) -> Optional<T> {
     x
+}
+
+func id<T>(param: T) -> T {
+    return param
 }
 
 let fortytwo = 42
@@ -18,7 +27,19 @@ let foo = pure(fortytwo)
 print(fortytwo)
 print(foo)
 
-let addTwo = add(2)
 
-print(addTwo(3))
-print(addTwo(5))
+// Apply: E<(a->b)> -> E<a> -> E<b>
+// In the option case here
+// (a->b) option -> a option -> b option
+// The apply function for Options
+// https://github.com/nrkno/fsharpskolen/blob/master/ddd-fsharp/functional/apply.fsx#L48
+func apply<A,B>(fOpt: ((A) -> (B))?) -> (_ xOpt: A?) -> (B?) 
+{
+    { xOpt in
+        switch (fOpt, xOpt) {
+            case (.some(let f), .some(let x)):
+                return f(x) 
+            case (_,_):
+                return .none
+    }
+}
