18b765d66bf6f4582f91078fd0a99928ec8c5dad
Bjørn Einar Bjartnes <bjartnes@gmail.com>
2021-04-09 19:01:34 +0200

    pure worked

--- Sources/hello/main.swift
+++ Sources/hello/main.swift
@@ -5,6 +5,9 @@ func add(_ x: Int) -> (_ y: Int) -> Int {
     { y in return x + y }
 }
 
+// https://github.com/nrkno/fsharpskolen/blob/master/ddd-fsharp/functional/apply.fsx#L32
+// This is option.pure but I do not know how to make an option module in swift yet 
+
 func pure<T>(_ x: T) -> Optional<T> {
     x
 }
