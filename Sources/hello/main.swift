eb9f81e07bf9b2480d451ed091b48b81ed78437d
Bjørn Einar Bjartnes <bjartnes@gmail.com>
2021-04-10 21:52:15 +0200

    need some bind examples now

--- Sources/hello/main.swift
+++ Sources/hello/main.swift
@@ -107,17 +107,18 @@ func ListBind<A,B>(_ f: @escaping ((A) -> Array<(B)>)) -> (_ xs: Array<A>) -> (A
     }
 }
 
-// We do not need to do this stuff anymore
+// We do not need to do this stuff anymore, because we have higher order
+// functions that can do it for us
 func parseNumbers (_ s: Array<String>) -> [Optional<Int>] {
     return s.compactMap{ Int($0)} 
 }
 
-// We can refactor to a simple function
+// So we can instead make a simple function that does it
 func parseNumber (_ nr: String) -> Optional<Int> {
     return Int(nr)
 }
 
-// and just lift the function
+// and just lift the function to work on lists
 let parseNumbers2 = ListMap(parseNumber)
 
 let someNumbers = ["2", "To", "4", "Fire"]
@@ -125,4 +126,6 @@ let someNumbers = ["2", "To", "4", "Fire"]
 print(parseNumbers(someNumbers))
 print(parseNumbers2(someNumbers))
 
-// We also made a 
+// We also made a function that we lifted from summing numbers to summing optional numbers. We can lift that one to work on lists as well
+let maybeTwoerListerAdder = ListMap(maybeTwoerAdder) 
+print(maybeTwoerListerAdder(parseNumbers2(someNumbers)))
